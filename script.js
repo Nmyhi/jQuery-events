f850b6d965c376869abb830a7cb46827bb0ee5b1
Mihaly Lovrencsics <lomysa@gmail.com>
2023-09-11 21:34:27 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+	$("p").on("click" , function() {
+		$("p").toggleClass("red");
+		 });
+	$("h2").mouseenter( function () {
+		$(this).css("font-size", "2em");
+	});
+	$("h2").mouseleave( function () {
+		$(this).css("font-size", "1em");
+	});
+	$("a:contains('Button')").mouseenter(function () {
+		$("body").css("background-color", "pink");
+	});
+	$("a:contains('Button')").mouseleave(function () {
+		$("body").css("background-color", "grey");
+	});
+
+
+}); 
